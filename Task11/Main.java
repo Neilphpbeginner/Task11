a360a4cfd14ca9efdaa6027c8e83b3cd165cebd2
Neilphpbeginner <lemmer.neil@gmail.com>
2017-12-21 17:16:07 +0200

    My Branch Test 2

--- Task11/Main.java
+++ Task11/Main.java
@@ -6,8 +6,7 @@ public class Main {		   // Defining the class
 //	Declaring the main method
 
 	public static void main(String[] args) {
-		
-<<<<<<< HEAD
+
 		Lion myLion 	= 	new Lion(20, true, 60, 5);  		// Declaring the first instance of the class lion
 		Lion myLion2	=	new Lion(18, false, 82, 4); 		// Declaring the second instance of the class lion
 		Lion myLion3	=	new	Lion(5, true, 150, 8);  		// Declaring the third instance of the class lion
@@ -15,15 +14,14 @@ public class Main {		   // Defining the class
 		myCheetah.setName("Neil");
 		
 //		Printing out the toString Method
-=======
-		Lion myLion 		= 	new Lion(20, true, 60, 5);  // Declaring the first instance of the class lion
-		Lion myLion2		=	new Lion(18, false, 82, 4); // Declaring the second instance of the class lion
-		Lion myLion3		=	new	Lion(5, true, 150, 8);  // Declaring the third instance of the class lion
-		Cheetah myCheetah 	=	new Cheetah(12, true, 25);
+
+		Lion myLion 		= 	new Lion(20, true, 60, 5);  	// Declaring the first instance of the class lion
+		Lion myLion2		=	new Lion(18, false, 82, 4); 	// Declaring the second instance of the class lion
+		Lion myLion3		=	new	Lion(5, true, 150, 8);  	// Declaring the third instance of the class lion
+		Cheetah myCheetah 	=	new Cheetah(12, true, 25);		// Declating a new instance of the class cheetah
 		myCheetah.setName("Neil");
 		
 //		Printing out the overided toString Method
->>>>>>> 8598b4407795a697c6f3df0e4b12f05904ef7abe
 		
 		System.out.println(myLion.toString());
 		System.out.println(myLion2.toString());
